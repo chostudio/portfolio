0b84f261bc972b3c695dd50d738919eaa1f1772a
Chris H <officialchrisho@gmail.com>
2023-06-15 08:49:03 +0800

    made new physics boxes

--- something.js
+++ something.js
@@ -26,10 +26,12 @@ function init() {
     // create two boxes and a ground
     var boxA = Bodies.rectangle(400, 200, 80, 80);
     var boxB = Bodies.rectangle(450, 50, 80, 80);
+    var boxC = Bodies.rectangle(100, 100, 80, 80);
+    var boxD = Bodies.rectangle(350, 400, 80, 80);
     var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
     // add all of the bodies to the world
-    Composite.add(engine.world, [boxA, boxB, ground]);
+    Composite.add(engine.world, [boxA, boxB, boxC, boxD, ground]);
 
     // run the renderer
     Render.run(render);
